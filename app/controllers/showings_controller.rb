decafcded4e70ca3ac639fe9f27dc445a8ed6502
lukesherwood <luke.sherwood55@gmail.com>
2021-03-10 09:37:47 -0500

    update and edit added for showings

--- app/controllers/showings_controller.rb
+++ app/controllers/showings_controller.rb
@@ -1,2 +1,21 @@
 class ShowingsController < ApplicationController
+    def edit
+        @showing = Showing.find(params[:id])
+        @auditoriums = Auditorium.all
+    end
+
+    def update
+        @showing = Showing.find(params[:id])
+        if @showing.update(showing_params)
+            redirect_to :users_admin, notice: "Successfully updated showing"
+        else 
+            render "edit"
+        end
+    end
+
+    private
+
+    def showing_params
+        params.require(:showing).permit(:auditorium_id, :time, :movie_id)
+    end
 end

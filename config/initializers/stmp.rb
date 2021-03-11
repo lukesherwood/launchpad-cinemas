82dc3bfde0ca4df1ec098915bd569dbc10f06517
lukesherwood <luke.sherwood55@gmail.com>
2021-03-11 12:04:29 -0500

    adding sendgrid email to production

---
+++ config/initializers/stmp.rb
@@ -0,0 +1,8 @@
+ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    authentication: :login,
+    enable_starttls_auto: true
+  }

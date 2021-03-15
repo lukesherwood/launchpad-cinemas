3a278da9d100e6af90c86c2a99c8584ebc21a663
lukesherwood <luke.sherwood55@gmail.com>
2021-03-15 11:56:21 -0400

    changed production mailer setup git push

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,5 +1,4 @@
 class OrderMailer < ApplicationMailer
-    default :from => "cinemas@launchpad.com"
     def order_confirmation
         @user = params[:user]
         mail(:to => "#{@user.name} <#{@user.email}>", :subject => "Order Confirmation")
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -131,6 +131,4 @@ Rails.application.configure do
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
-
-  config.require_master_key = true
 end
